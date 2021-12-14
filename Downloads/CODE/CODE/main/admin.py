9bf4a16f356c6fa039891fb32488d405e5441919
rollerntroller <95750970+rollerntroller@users.noreply.github.com>
2021-12-15 01:28:02 +0530

    Delete Downloads directory

--- Downloads/CODE/CODE/main/admin.py
+++
@@ -1,9 +0,0 @@
-# Register your models here.
-from django.contrib.auth.admin import UserAdmin
-from django.contrib import admin
-from main.models import WebsiteUser
-from django.contrib.flatpages.admin import FlatPageAdmin
-from django.contrib.flatpages.models import FlatPage
-
-admin.site.register(WebsiteUser, UserAdmin)
-# admin.site.register(FlatPage, FlatPageAdmin)
